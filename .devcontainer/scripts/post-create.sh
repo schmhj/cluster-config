d89abfbeec591afab3ddf556b235cab077fe4ec2
Sachindra Maharjan <118582768+sachindra-maharjan@users.noreply.github.com>
2026-02-03 17:56:42 +0000

    Delete install files

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -25,4 +25,7 @@ curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.34
 tar -xvzf kubeseal-0.34.0-linux-amd64.tar.gz kubeseal  | tee -a ~/.status.log
 sudo install -m 755 kubeseal /usr/local/bin/kubeseal  | tee -a ~/.status.log
 
+rm kubeseal-0.34.0-linux-amd64.tar.gz
+rm kubeseal
+
 echo "post-create complete" >> ~/.status.log
