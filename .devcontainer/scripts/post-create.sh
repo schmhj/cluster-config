bcb6f70cbf5d983800fe0eb904c44af3beec8b17
Sachindra Maharjan <118582768+sachindra-maharjan@users.noreply.github.com>
2026-03-10 15:38:36 +0000

    project restructuring

--- .devcontainer/scripts/post-create.sh
+++ .devcontainer/scripts/post-create.sh
@@ -42,20 +42,22 @@ helm repo update | tee -a  ~/.status.log
 helm install argocd argo/argo-cd --version 7.8.26 --namespace argocd --create-namespace --set server.service.type="NodePort" --set server.service.nodePortHttps=30179 --set configs.cm."kustomize\.buildOptions"="--enable-helm" --set configs.cm."application\.sync\.impersonation\.enabled"="true" | tee -a  ~/.status.log 
 
 # Install Sealed Secrets
-helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets | tee -a ~/.status.log
-helm install sealed-secrets sealed-secrets/sealed-secrets --namespace kube-system --set existingSecret="$SECRETS_PRIV_KEY" | tee -a ~/.status.log
+# helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets | tee -a ~/.status.log
+# helm install sealed-secrets sealed-secrets/sealed-secrets --namespace kube-system --set existingSecret="$SECRETS_PRIV_KEY" | tee -a ~/.status.log
 
 # Install emberstack to reflect secrets across namespaces
-helm repo add emberstack https://emberstack.github.io/helm-charts
-helm repo update
-helm install reflector emberstack/reflector --namespace kube-system
+# helm repo add emberstack https://emberstack.github.io/helm-charts
+# helm repo update
+# helm install reflector emberstack/reflector --namespace kube-system
 
 # Install kubeseal
-curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.34.0/kubeseal-0.34.0-linux-amd64.tar.gz" | tee -a ~/.status.log
-tar -xvzf kubeseal-0.34.0-linux-amd64.tar.gz kubeseal  | tee -a ~/.status.log
-sudo install -m 755 kubeseal /usr/local/bin/kubeseal  | tee -a ~/.status.log
+# curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.34.0/kubeseal-0.34.0-linux-amd64.tar.gz" | tee -a ~/.status.log
+# tar -xvzf kubeseal-0.34.0-linux-amd64.tar.gz kubeseal  | tee -a ~/.status.log
+# sudo install -m 755 kubeseal /usr/local/bin/kubeseal  | tee -a ~/.status.log
 
-rm kubeseal-0.34.0-linux-amd64.tar.gz
-rm kubeseal
+# rm kubeseal-0.34.0-linux-amd64.tar.gz
+# rm kubeseal
+
+rm custom-sealed-secret-key.yaml
 
 echo "post-create complete" >> ~/.status.log
